a4c9c3fd9a99729a6f992d26c05a9d30a3e4c564
Oskar Lang <oskar.risto.lang@gmail.com>
2021-03-30 18:26:34 +0200

    feat: add deferred shutdown of web server in gateway service

--- services/gateway_service/main.go
+++ services/gateway_service/main.go
@@ -43,6 +43,15 @@ func main() {
 		}
 	}()
 
+	// Defer shutting down of fiber server
+	defer func() {
+		// Try to gracefully shutdown webserver
+		err = app.Shutdown()
+		if err != nil {
+			zap.L().Fatal("failed to gracefully shutdown webserver", zap.Error(err))
+		}
+	}()
+
 	// Await interruption signal in order to gracefully shutdown webserver
 	stop := make(chan os.Signal)
 	signal.Notify(stop, os.Interrupt)
